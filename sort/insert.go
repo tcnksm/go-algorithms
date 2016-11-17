e1faee1afb4b81180a709806090047030bb32ca8
tcnksm <nsd22843@gmail.com>
2016-11-17 17:20:57 +0900

    Add documents

--- sort/insert.go
+++ sort/insert.go
@@ -3,6 +3,8 @@ package sort
 // Insert sorts https://en.wikipedia.org/wiki/Insertion_sort
 func Insert(a []int) {
 	for i := 0; i < len(a); i++ {
+		// Get element one by one and compare with elements
+		// which are already sorted.
 		j := i
 		for j > 0 && a[j-1] > a[j] {
 			a[j-1], a[j] = a[j], a[j-1]
