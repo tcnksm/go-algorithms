2105dde32a68c2d6783b286e2e61b6e4f9c7ca13
tcnksm <nsd22843@gmail.com>
2016-11-17 17:37:14 +0900

    Multiple benchmark by sub-benchmark

--- sort/benchmark_test.go
+++ sort/benchmark_test.go
@@ -1,16 +1,48 @@
 package sort
 
-import "testing"
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"testing"
+)
 
-func BenchmarkInsert1K(b *testing.B) {
-	b.StopTimer()
-	for i := 0; i < b.N; i++ {
-		data := make([]int, 1<<10)
-		for i := 0; i < len(data); i++ {
-			data[i] = i ^ 0x2cc
+func BenchmarkSort(b *testing.B) {
+	cases := []struct {
+		Name string
+		Func func([]int)
+	}{
+		{
+			"Standard",
+			sort.Ints,
+		},
+
+		{
+			"Insert",
+			Insert,
+		},
+
+		{
+			"Bubble",
+			Bubble,
+		},
+	}
+	sizes := []int{1000, 5000, 10000}
+	for _, size := range sizes {
+		for _, bc := range cases {
+			b.Run(fmt.Sprintf("%s-%d", bc.Name, size), func(b *testing.B) {
+				b.StopTimer()
+				for i := 0; i < b.N; i++ {
+					data := make([]int, size)
+					for i := 0; i < len(data); i++ {
+						data[i] = rand.Intn(size)
+					}
+					b.StartTimer()
+					bc.Func(data)
+					b.StopTimer()
+				}
+			})
 		}
-		b.StartTimer()
-		Insert(data)
-		b.StopTimer()
+
 	}
 }
