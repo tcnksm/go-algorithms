4ceef6c91f55da7a8fbed59130f934e70676dffb
tcnksm <nsd22843@gmail.com>
2016-11-20 17:36:44 +0900

    Refactoring

--- sort/insert.go
+++ sort/insert.go
@@ -5,10 +5,8 @@ func Insert(a []int) {
 	for i := 0; i < len(a); i++ {
 		// Get element one by one and compare with elements
 		// which are already sorted.
-		j := i
-		for j > 0 && a[j-1] > a[j] {
+		for j := i; j > 0 && a[j-1] > a[j]; j-- {
 			a[j-1], a[j] = a[j], a[j-1]
-			j--
 		}
 	}
 }
