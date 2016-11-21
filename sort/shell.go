fcd4b755fc6c2884104db5ec219c8af7ebb4f2e6
tcnksm <nsd22843@gmail.com>
2016-11-21 18:58:55 +0900

    Fix shell sort

--- sort/shell.go
+++ sort/shell.go
@@ -5,7 +5,7 @@ func Shell(a []int) {
 	for h := len(a) / 2; h > 0; h /= 2 {
 		for i := h; i < len(a); i++ {
 			for j := i; j >= h && a[j-h] > a[j]; j -= h {
-				a[j-1], a[j] = a[j], a[j-1]
+				a[j-h], a[j] = a[j], a[j-h]
 			}
 		}
 	}
