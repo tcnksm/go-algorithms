17fd312159343c5cf8c06b0b20f5906f8ccd6643
tcnksm <nsd22843@gmail.com>
2016-11-23 00:01:54 +0900

    Tuning

--- sort/shell.go
+++ sort/shell.go
@@ -2,7 +2,7 @@ package sort
 
 // Shell sort is improvement of Insert sort
 func Shell(a []int) {
-	for h := len(a) / 2; h > 0; h /= 2 {
+	for h := len(a) / 3; h > 0; h /= 3 {
 		for i := h; i < len(a); i++ {
 			for j := i; j >= h && a[j-h] > a[j]; j -= h {
 				a[j-h], a[j] = a[j], a[j-h]
